4a96a55f78c95d530a95846b2b7e840102e433cd
bhargav-yarlagadda <bhargavyarlagadda2003@gmail.com>
2024-10-08 09:48:17 +0530

    chore

--- src/context/PlaygroundContext.jsx
+++ src/context/PlaygroundContext.jsx
@@ -1,16 +1,60 @@
-import React from 'react'
-import { createContext } from 'react'
+import React, { useEffect, useState, createContext } from 'react'
+import { v4 } from 'uuid'
 
-export const PlaygroundContextProvider = createContext()
-
-const PlaygroundContext = ({children}) => {
-  return (
-   <PlaygroundContext.Provider  >
+// Initial Data
+const initialData = [
     {
-        children
+        id: v4(),
+        title: "DSA",
+        files: [
+            {
+                id: v4(),
+                title: 'Introduction to CPP',
+                language: "cpp", 
+                code: `
+                #include <iostream> 
+                int main()
+                {
+                    std::cout << "Hello World";
+
+                    return 0;
+                }
+                `
+            }
+        ]
+    },
+    {
+        id: v4(),
+        title: "Front End",
+        files: [
+            {
+                id: v4(),
+                title: 'Introduction to JS',
+                language: "javascript", 
+                code: `
+                console.log("Hello World");
+                `
+            }
+        ]
     }
-   </PlaygroundContext.Provider>
-  )
+]
+
+// Create context
+export const PlaygroundContext = createContext()
+
+// PlaygroundContextProvider component
+const PlaygroundContextProvider = ({ children }) => {
+    const [folders, setFolders] = useState(initialData)
+
+    useEffect(() => {
+        localStorage.setItem("data", JSON.stringify(folders))
+    }, [folders])  // Ensure to save to localStorage whenever folders change
+
+    return (
+        <PlaygroundContext.Provider value={{ folders, setFolders }}>
+            {children}
+        </PlaygroundContext.Provider>
+    )
 }
 
-export default PlaygroundContext
+export default PlaygroundContextProvider
